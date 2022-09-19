84bb4b43afcddc6fe38bdc17bffd6e17d4a0c904
Tuan Le <tuanz_z@yahoo.com>
2022-09-19 08:20:08 -0700

    final assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
 
+// const { number } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,14 +16,31 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
+// PSEUDOCODE: We invoke the describe/it method with people as the argument. In plain words we explain what the function does, "takes in an array of objects and returns an array with a sentence about each person with their name capitalized". We then provide an argument for the function to call on. Using toEqual we then chain it with the expected output.
+
 const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
 ]
+
+describe("bigPeople", () => {
+  it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized", () => {
+    expect(bigPeople(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+    )
+  })
+})
+
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
 // b) Create the function that makes the test pass.
+// Pseudocode: we declare a function named bigPeople that takes in an object as its parameter. Using .split method were going to split the 'name' key into an array and capitalize the 0 index with .toUpperCase. Now that we have the first letter capitalized we add it to the sliced array of words so together they will form the key with capitalized words. Then we add it to the outputted string using interpolation. 
+
+function bigPeople(object) {
+  let fullSentence = object.map(object => object.name.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ") + ` is ${object.occupation}.`)
+  return fullSentence
+}
+
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
@@ -32,7 +51,20 @@ const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+describe("remainder", () => {
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+    expect(remainder(hodgepodge1)).toEqual([2, 0, -1, 0])
+    expect(remainder(hodgepodge2)).toEqual([2, 1, -1])
+  })
+})
+
 // b) Create the function that makes the test pass.
+// Pseudocode: We create a function named remainder and set the parameters to arr. Inside the function we declare a variable div3 and its going to equal the arr.filter so we can filter every element that is strictly equal to an number to a new array. We then return div3.map so that we can iterate through the whole array and return every number modulo 3. ez
+
+const remainder = (arr) => {
+  let div3 = arr.filter(value => typeof value === 'number')
+  return div3.map(arr => arr % 3)
+}
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
@@ -43,4 +75,22 @@ const cubeAndSum1 = [2, 3, 4]
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe("cubeAndSum", () => {
+  it("takes in an array of numbers and returns the sum of all the numbers cubed", () => {
+    expect(cubeAndSum(cubeAndSum1)).toEqual(99)
+    expect(cubeAndSum(cubeAndSum2)).toEqual(1125)
+  })
+})
+
 // b) Create the function that makes the test pass.
+// Pseudocode: Create ba function named cubeAndSum that takes in array as its parameter. Declare a variable named cubed and its gonna equal the mapped array with their values to the power of 3. Declare a variable named sum and set it to 0. We're gonna run a for loop on cubed and add each value to sum( which starts with 0) and this should return us the sum of every number included in the array named cubed. 
+
+function cubeAndSum(array) {
+  let sum = 0
+  let cubed = array.map(value => value ** 3)
+  for (let i = 0; i < cubed.length; i++) {
+    sum += cubed[i]
+  }
+  return sum
+}
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,36 +8,64 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
+Your answer: To modify a model already generated we would either need to edit it or migrate it. Since student belongs to the cohort, the foreign key would be on the student model because the primary key is associated with the cohort model.
 
-Researched answer:
+Researched answer: So we would fix this using active record migration. After migrating , rails will update the schema with a foreign key adding it to students. 
 
 2. Which RESTful routes must always be passed params? Why?
 
-Your answer:
+Your answer: Every route needs a parameter except for the new method. The parameter acts like an ID for every object, it's important to have parameters because it locates what we are looking for. For example if we want to delete or edit a certain object we located it by its ID. 
 
-Researched answer:
+Researched answer: Without passing parameters, we as developers would not be able to perform actions on a specific instance that we are looking for. Therefor methods that require an ID to locate a specific instance would be show, edit, update and destroy.
 
 3. Name three rails generator commands. What is created by each?
 
 Your answer:
+1. rails generate model - creates a boilerplate model
+2. rails new - creates a new Rails application 
+3. rail generate resource - creates RESTful routes and files for the db.
 
 Researched answer:
+1. rails new interview_questions -d postgresql -T => creates a new app and specifies that postgresql is the database of the app
+2. rails g resource => generates a resource that you want to create along with the table column names and types.
+3. rails g migration => creates a change on the database schema.
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
-action: "GET" location: /students
+action: "GET" location: /students => This shows the index of all students. 
 
-action: "POST" location: /students
+action: "POST" location: /students => Creates a new instance of the model. 
 
-action: "GET" location: /students/new
+action: "GET" location: /students/new => This creates a new student.
 
-action: "GET" location: /students/2
+action: "GET" location: /students/2 => This shows us the student at ID #2.
 
-action: "GET" location: /students/2/edit
+action: "GET" location: /students/2/edit => This edits the student at ID #2.
 
-action: "PATCH" location: /students/2
+action: "PATCH" location: /students/2 => This updates the student at ID #2.
 
-action: "DELETE" location: /students/2
+action: "DELETE" location: /students/2 => After all we did to student #2, DELETE will destroy student #2 from the db. 
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+User Stories:
+
+1. As a developer I want to create a new app called to-do-list thats tracks my everyday chores
+ 
+2. As a developer I want to have a task name, date, time, complete
+ 
+3. As a developer I can to see all the instances on my to do list
+ 
+4. As a developer I can create new chores for my list
+ 
+5. As a developer I can edit the chores on my list 
+ 
+6. As a developer I want to be able to find a specific chore on my list
+ 
+7. As a developer I want to be able to remove a task
+ 
+8. As a developer I can update a chore on my list
+ 
+9. As a developer I can check if a chore is complete or not
+ 
+10. As a developer I can add a duration to my chores
